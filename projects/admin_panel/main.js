85a8ee6b50199015c0986720b7f98bb2c4c9b241
aShinde <ananta.shinde@honeywellcom>
2023-10-26 10:58:14 +0530

    26/10/23

--- projects/admin_panel/main.js
+++ projects/admin_panel/main.js
@@ -1,5 +1,21 @@
 
-const saveBtn = document.getElementById("save");
+// form is hidden by default
+var addNewForm = document.getElementById("add-new-product-form");
+addNewForm.setAttribute("hidden",true)
+
+var productTable = document.getElementById("product-table");
+
+const addNewClicked = ()=>{
+  console.log("add clicked")
+     addNewForm.removeAttribute("hidden");
+     productTable.setAttribute("hidden",true)
+}
+const viewProductClicked = ()=>{
+  console.log("add clicked")
+  productTable.removeAttribute("hidden");
+  addNewForm.setAttribute("hidden",true)
+}
+
 
 function clicked(){
    var pname =  document.getElementById("name").value;
@@ -17,3 +33,33 @@ function clicked(){
    products.push(newProduct);
    localStorage.setItem("products",JSON.stringify(products))
 }
+
+//create product row
+const createProductRow = (p) => {
+  var tr = document.createElement("tr");
+  //get keys array
+  keys = Object.keys(p);
+  for(i=0;i<3;i++)
+  {
+       var td = document.createElement("td");
+       td.innerHTML = p[keys[i]];
+       tr.appendChild(td)
+  }
+  var td = document.createElement("td");
+  var btn = document.createElement("button");
+  btn.innerHTML = "delete"
+  td.appendChild(btn)
+  tr.appendChild(td)
+  productTable.appendChild(tr);
+  }
+  
+
+
+// adding data row to table
+ 
+
+products = JSON.parse(localStorage.getItem("products"));
+
+products.map(p=>createProductRow(p));
+ 
+
