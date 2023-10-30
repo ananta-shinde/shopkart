5be27c2b6aef861ab259732642a812021309eebf
aShinde <ananta.shinde@honeywellcom>
2023-10-30 11:25:04 +0530

    30/10/23

--- projects/admin_panel/main.js
+++ projects/admin_panel/main.js
@@ -29,7 +29,7 @@ function clicked(){
      products = Array(); 
    }
    
-   var newProduct = {name:pname,price:pprice,category:pcat}
+   var newProduct = {id:Math.floor(Math.random() * 100), name:pname,price:pprice,category:pcat}
    products.push(newProduct);
    localStorage.setItem("products",JSON.stringify(products))
 }
@@ -37,21 +37,56 @@ function clicked(){
 //create product row
 const createProductRow = (p) => {
   var tr = document.createElement("tr");
+  tr.setAttribute("pid",p.id);
   //get keys array
   keys = Object.keys(p);
+
+  // creating column for each key
   for(i=0;i<3;i++)
   {
        var td = document.createElement("td");
-       td.innerHTML = p[keys[i]];
+       td.innerHTML = p[keys[i+1]];
        tr.appendChild(td)
   }
   var td = document.createElement("td");
   var btn = document.createElement("button");
   btn.innerHTML = "delete"
+  btn.classList ="btn btn-danger";
+  btn.setAttribute("id",p.id);
+  btn.addEventListener("click",deleteClicked);
+  td.appendChild(btn)
+  tr.appendChild(td)
+  var btn = document.createElement("button");
+  btn.innerHTML = "update"
+  btn.classList ="btn btn-info";
+  btn.setAttribute("id",p.id);
+  btn.addEventListener("click",updateClicked);
   td.appendChild(btn)
   tr.appendChild(td)
   productTable.appendChild(tr);
   }
+
+  const deleteClicked = (e) =>{
+    var pid = e.target.id;
+    products = JSON.parse( localStorage.getItem("products"))
+    products = products.filter(p=>p.id != pid );
+    localStorage.setItem("products",JSON.stringify(products))
+    location.reload()
+  }
+
+  const updateClicked = (e)=>{
+    var modal = document.getElementById("update-product-form");
+    var updatebtn = document.getElementById("update");
+    // updatebtn.value = e.target.id;
+    modal.style.display = "block";
+  }
+
+  const update = ()=>{
+         console.log(e);
+  }
+
+
+
   
 
 
