a43cc0e52d0ac8ab73b816499b185f572c478c2d
aShinde <ananta.shinde@honeywellcom>
2023-12-08 11:45:38 +0530

    08/12/23

--- react-demos/shopkart/src/pages/login.jsx
+++ react-demos/shopkart/src/pages/login.jsx
@@ -1,18 +1,38 @@
 import './login.css'
 const Login = () => {
+    let email;
+    let password;
+
+    const handlechange = (e) =>{
+        if(e.target.name == "email")
+        email = e.target.value
+
+        if(e.target.name == "password")
+        password = e.target.value
+    }
+
+
+   const handleSubmit = async (e) =>{
+         e.preventDefault()
+         const response = await fetch("http://localhost:5000/users",{
+            "method": "get",    
+        });
+        console.log(response.json());
+   }
+
     return ( 
     <div className='login-form-container'>
         <form className='shadow bg-light border'>
             <h4 className='text-center m-2'>Welcome Back ! Please Login</h4>
             <div className="form-group">
                  
-                <input className="form-control" type="text" placeholder='username'/>
+                <input className="form-control" type="text" placeholder='username' name="email" onChange={handlechange}/>
             </div>
             <div className="form-group">
  
-                <input className="form-control" type="password" placeholder='password'/>
+                <input className="form-control" type="password" placeholder='password' name="password" onChange={handlechange}/>
             </div>
-            <button className="btn btn-primary">Login</button>
+            <button className="btn btn-primary" onClick={handleSubmit}>Login</button>
         </form> 
     </div>);
 }
