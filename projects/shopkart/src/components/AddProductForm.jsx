51d85f4f2780c29b82a1c436ef35d078625f4950
aShinde <ananta.shinde@honeywellcom>
2024-01-02 11:08:20 +0530

    02/01/24

--- projects/shopkart/src/components/AddProductForm.jsx
+++ projects/shopkart/src/components/AddProductForm.jsx
@@ -1,7 +1,58 @@
+import { useEffect, useState } from "react";
+
 const AddProductForm = () => {
-    return ( <>
-         Add new Product
-    </> );
+    const [categories,setCategories] = useState([]);
+    const [brands,setBrands] = useState([]);
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/categories",{ method: "Get"})
+        .then(res=> res.json())
+        .then(data =>{
+             setCategories(data);
+        })
+
+        fetch("http://localhost:5000/brands",{ method: "Get"})
+        .then(res=> res.json())
+        .then(data =>{
+             setBrands(data);
+        })
+    },[])
+    return ( <div className="p-4">
+         <h4>Add new product</h4>
+         <form>
+            <div className="form-group">
+                <label>Product Name :</label>
+                <input className="form-control" type="text" placeholder="Ex. Laptop 15455"/>
+            </div>
+            <div className="form-group">
+                <label>Product Price :</label>
+                <input className="form-control" type="text" placeholder="Ex. 45000"/>
+            </div>
+            <div className="form-group">
+                <label>Product Category :</label>
+                 <select className = "form-control" name="" id="">
+                    <option value="0">-- select category --</option>
+                    {
+                        categories.map(c=>(<option value={c.id}>{c.name}</option>))
+                    }
+                 </select>
+            </div>
+            <div className="form-group">
+                <label>Product Brand :</label>
+                 <select className = "form-control" name="" id="">
+                    <option value="0">-- select brand --</option>
+                    {
+                        brands.map(b=>(<option value={b.id}>{b.name}</option>))
+                    }
+                 </select>
+            </div>
+            <div className="form-group">
+                <label>Product Image :</label>
+                <input className="form-control" type="url" placeholder="please provide URL"/>
+            </div>
+            <button className="btn btn-success">Submit</button>
+         </form>
+    </div> );
 }
  
 export default AddProductForm;
--- projects/shopkart/src/components/ProductList.jsx
+++ projects/shopkart/src/components/ProductList.jsx
@@ -1,6 +1,41 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const ProductList = () => {
+    const [products,setProducts] = useState();
+    let keys = [];
+    if(products)
+    {
+        keys = Object.keys(products[0])
+    }
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/products",{
+            method:"Get",
+        }).then(res=>res.json())
+        .then(data =>{
+            setProducts(data);
+        });
+       
+    },[])
+
     return ( <>
-         ProductList
+    { products ?
+          <table className="table">
+          <tr>
+              {
+                 keys.map(k=>(<th>{k}</th>))
+              }
+          </tr>
+          {
+             products.map(p=>( <tr>
+                  { keys.map(k=>(<td>{p[k]}</td>))}
+              </tr>))
+          }
+       </table>:
+       <p> No data to show</p>
+    }
+          
     </> );
 }
  
